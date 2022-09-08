dbaf6bd33048a9c5f0f68ed9b88df1c38e11f8e8
Anz010 <anzarovanzor@gmail.com>
2022-09-07 20:56:03 -0400

    code added

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,26 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
+
+public class LoginPage extends CommonMethods {
+
+    @FindBy(id = "txtUsername")
+    public WebElement usernameTextField;
+
+    @FindBy(id = "txtPassword")
+    public WebElement passwordTextField;
+
+    @FindBy(id = "btnLogin")
+    public WebElement loginButton;
+
+    @FindBy(id = "spanMessage")
+    public WebElement errorMessage;
+
+    public LoginPage(){
+        PageFactory.initElements(driver, this);
+    }
 }
+
